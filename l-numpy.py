c55ef90e099e53eb111c18e2ba8bbd437fcdd779
Anurag Soni <anurag.soni@indiamart.com>
2025-06-06 12:46:40 +0530

    first commit

---
+++ l-numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Element-wise operations
+# print("a + b:", a + b)
+# print("Dot product:", np.dot(a, b))
+# print("Cross product:", np.cross(a, b))
+# print(np.shape(a))
+
+a = np.array([[1, 2, 3],[4, 5, 6]])
+print(a)
